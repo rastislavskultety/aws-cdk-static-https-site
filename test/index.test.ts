0f468a700a9ec92f1cc27d5926ac1b7114e04b16
Rasto Skultety <rskultety@gmail.com>
2021-08-08 00:10:58 +0200

    test coverage

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,15 +5,13 @@ import '@aws-cdk/assert/jest';
 
 
 test('StaticWebSite', () => {
-  // const app = new App();
-  // const stack = new Stack(app, 'test');
   const stack = new Stack();
 
   const site = new StaticWebSite(stack, 'MyStaticWebSite', {
     domainName: 'example.org',
     siteSubDomain: 'www',
     siteContentsPath: './test/website',
-    certificateArn: '...',
+    certificateArn: 'someArn',
     websiteIndexDocument: 'main.html',
     websiteErrorDocument: '404.html',
   });
@@ -24,6 +22,28 @@ test('StaticWebSite', () => {
     WebsiteConfiguration: {
       ErrorDocument: '404.html',
       IndexDocument: 'main.html',
+    },
+  });
+  expect(stack).toHaveResource('AWS::CloudFront::Distribution');
+  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+});
+
+test('Default documents', () => {
+  const stack = new Stack();
+
+  const site = new StaticWebSite(stack, 'MyStaticWebSite', {
+    domainName: 'example.org',
+    siteSubDomain: 'www',
+    siteContentsPath: './test/website',
+    certificateArn: 'someArn',
+  });
+
+  expect(site.siteDomain).toBe('www.example.org');
+  expect(stack).toHaveResource('AWS::S3::Bucket', {
+    BucketName: 'www.example.org',
+    WebsiteConfiguration: {
+      ErrorDocument: 'index.html',
+      IndexDocument: 'index.html',
     },
   });
   expect(stack).toHaveResource('AWS::CloudFront::Distribution');
